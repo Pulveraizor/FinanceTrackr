3cd217303a52347e609dd18aff52a4db55d6a532
benas-juskus <benas.juskus@stud.ktmc.lt>
2024-01-21 16:46:24 +0200

    First commit, login page for mobile complete

---
+++ client/src/pages/Login.js
@@ -0,0 +1,47 @@
+function Logo() {
+    return (
+        <img class="rounded-circle" src="finlogo.jpg" alt="logo"></img>
+    )
+}
+
+function InputUsername() {
+    return (
+        <div className="mt-3">
+            <label for="username" className="text-light">Username:</label>
+            <input type="text" id="username" className="form-control"></input>
+        </div>
+    )
+}
+function InputPassword() {
+    return (
+        <div className="mt-3">
+            <label for="password" className="text-light">Password:</label>
+            <input type="password" id="password" className="form-control"></input>
+        </div>
+    )
+}
+
+function SubmitBtn() {
+    return <button type="submit" className="text-light btn bg-main-dark-blue mt-5">Login</button>
+}
+
+function LoginForm() {
+    return (
+
+ 
+        <form className="row" action="/auth" method="post">
+            <div className="col-12 card bg-transparent border-0 p-5">
+                <>
+                    <Logo />
+                    <InputUsername />
+                    <InputPassword />
+                    <SubmitBtn />
+                </>
+            </div>
+        </form>
+
+ 
+    )
+}
+
+export default LoginForm;
---
+++ client/src/pages/layout/Footer.js
@@ -0,0 +1,5 @@
+function Footer() {
+    return <footer className="container-fluid footer text-light text-center bg-secondary container-fluid align-self-end p-3"> Finances Tracker || Wisemonks</footer>
+}
+
+export default Footer;
