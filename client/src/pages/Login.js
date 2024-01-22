b41809f245ee242d411c1d074842de4d46dfd698
benas-juskus <benas.juskus@stud.ktmc.lt>
2024-01-22 13:52:14 +0200

    Added express router basic functionality

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,22 +1,32 @@
 function Logo() {
     return (
-        <img class="rounded-circle" src="finlogo.jpg" alt="logo"></img>
+        <img className="rounded-circle" src="finlogo.jpg" alt="logo"></img>
     )
 }
 
 function InputUsername() {
     return (
-        <div className="mt-3">
-            <label for="username" className="text-light">Username:</label>
-            <input type="text" id="username" className="form-control"></input>
+        <div className="row input-group has-validation mt-3 mx-0">
+            <div className="px-0">
+                <label htmlFor="username" className="text-light form-label">Username:</label>
+                <input type="text" id="username" name="username" className="form-control" required></input>
+            </div>
+            <div className="invalid-feedback col-12">
+                Please enter username
+            </div>
         </div>
     )
 }
 function InputPassword() {
     return (
-        <div className="mt-3">
-            <label for="password" className="text-light">Password:</label>
-            <input type="password" id="password" className="form-control"></input>
+        <div className="row input-group has-validation mt-3 mx-0">
+            <div className="px-0">
+                <label htmlFor="password" className="text-light form-label">Password:</label>
+                <input type="password" id="password" name="password" className="form-control" required></input>
+            </div>
+            <div className="invalid-feedback col-12">
+                Please enter password
+            </div>
         </div>
     )
 }
@@ -29,7 +39,7 @@ function LoginForm() {
     return (
 
  
-        <form className="row justify-content-center" action="/auth" method="post">
+        <form className="row justify-content-center needs-validation" action="/auth" method="post">
             <div className="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-3 card bg-transparent border-0 p-5">
                 <>
                     <Logo />
