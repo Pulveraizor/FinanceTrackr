5dc1a432e7cd417a235dc0067df96c8a55fac75d
Pulveraizor <benas.juskus@gmail.com>
2024-02-14 09:39:41 +0200

    Added input fields for deposit details

--- client/src/pages/Deposit.jsx
+++ client/src/pages/Deposit.jsx
@@ -1,5 +1,46 @@
 // import React, { useState, useEffect } from 'react';
 
+
+function TransactionName() {
+    return(
+        <div className="px-0">
+            <label htmlFor="merchant_name" className="text-light form-label">Transaction name:</label>
+            <input type="text" id="merchant_name" name="merchant_name" className="form-control" required></input>
+        </div>
+    )
+}
+function MerchantGroup() {
+    return(
+        <div className="mb-3 px-0">
+            <label htmlFor="group" className="text-light form-label">Merchant group:</label>
+            <select className="form-select" id="group" name="merchant_group">
+                <option selected>Choose...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    )
+}
+function Amount() {
+    return(
+        <div className="px-0">
+            <label htmlFor="amount" className="text-light form-label">Amount:</label>
+            <input type="number" id="amount" name="amount" className="form-control" required></input>
+            <div className="invalid-feedback col-12">
+                Please enter a valid amount
+            </div>
+        </div>
+        
+    )
+}
+function SubmitBtn() {
+    return(
+        <div className="mt-3 px-0">
+            <button type="submit" className="w-100 btn btn-main-dark-blue">Deposit</button>
+        </div>
+    )
+}
 function DepositForm() {
     return (
         <div className="h-75 d-flex align-items-center justify-content-center bg-transparent">
@@ -9,16 +50,10 @@ function DepositForm() {
                     <div className="text-light">
                     <h1>Add funds:</h1>
                         <div className="row input-group has-validation mt-3 mx-0">
-                            <div className="px-0">
-                                <label htmlFor="amount" className="text-light form-label">Amount:</label>
-                                <input type="number" id="amount" name="amount" className="form-control" required></input>
-                            </div>
-                            <div className="mt-3 px-0">
-                                <button type="submit" className="w-100 btn btn-main-dark-blue">Deposit</button>
-                            </div>
-                            <div className="invalid-feedback col-12">
-                                Please enter a valid amount
-                            </div>
+                            <TransactionName />
+                            <MerchantGroup />
+                            <Amount />
+                            <SubmitBtn  />
                         </div>
                     </div>
                 </div>
